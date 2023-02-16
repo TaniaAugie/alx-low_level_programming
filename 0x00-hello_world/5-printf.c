f440f554ca01cba0e998de340e093a8752f2a7a7
TaniaAugie <taniaaugust@icloud.com>
2023-02-16 11:57:02 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,8 @@
  * main - program that prints line with printf function
  * return: 0
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
 	return (0);
 }
-
