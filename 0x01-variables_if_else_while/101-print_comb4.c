4c0a4d1d5ad24284ca31c4dbdff13b15b1421d5a
TaniaAugie <taniaaugust@icloud.com>
2023-02-20 09:23:49 -0800

    The success combination in business is

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - prints all possible different combinations of three digits
  * Return: Always 0
  */
 int main(void)
 {
-	int c;
-	int d = 0;
+	int digit1, digit2, digit3;
 
-	while (d < 10)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		c = 0;
-		while (c < 10)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			if (d != c && d < c)
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
-				putchar('0' + d);
-				putchar('0' + c);
-				if (c + d != 17)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
+
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
 			}
-			c++;
 		}
-		d++;
 	}
+
 	putchar('\n');
+
 	return (0);
 }
