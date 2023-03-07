0e2c1e985fac6fb460194b83486bb10f5a4a029a
TaniaAugie <taniaaugust@icloud.com>
2023-03-07 09:05:29 -0800

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,11 +2,29 @@
 # include <stdlib.h>
 /**
  * _strspn - gets the length of a prefix substring
- * @s: returns bytes in the initial segment
- * @accept: 
- * Return: the number of bytes in the initial segment of s which consist 
+ * @s: input
+ * @accept: input
+ * Return: the number of bytes in the initial segment of s which consist
  * only of bytes from @accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int n = 0;
+	int r;
 
+	while (*s)
+	{
+		for (r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
